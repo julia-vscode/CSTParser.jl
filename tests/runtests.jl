89773034912d0bd09dad0bfbd996dff3e8eb33d6
Zac Nugent <zac.nugent@gmail.com>
2017-01-27 14:59:06 +0000

    wip

--- tests/runtests.jl
+++ tests/runtests.jl
@@ -186,6 +186,37 @@ end
 
 
 
+const examplemodule = """
+module ModuleName
+const a = 16
+typealias MyInt Int
+bitstype a*4 SomeType
+
+f(x) = x
+
+function f end
+
+function f(x::Int{T})
+    while true
+        f(x)
+        while true
+            break
+        end
+    end
+
+    return x<4
+end
+
+type T <: Int
+    a::Int
+    b::Vector
+end
+
+baremodule SubModuleName
+end
+
+end
+"""
 
 function timetest(n)
     for i =1:n
