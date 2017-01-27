87f3122021264a5e9b5707bfe260beb7dd2e296a
Zac Nugent <zac.nugent@gmail.com>
2017-01-27 16:02:51 +0000

    fix display

--- src/display.jl
+++ src/display.jl
@@ -1,37 +1,37 @@
-function Base.show(io::IO, x::INSTANCE,indent=0, pos=0)
+function Base.show(io::IO, x::INSTANCE,indent=0)
     println(io, "⊢","-"^indent, "($(x.val))")
 end
-function Base.show(io::IO, x::CALL,indent=0, pos=0)
-    println(io, "⊢","-"^indent,"call")
-    cnt=0
+function Base.show(io::IO, x::CALL,indent=0) 
+    println(io, "⊢","-"^indent, x.name isa INSTANCE ? x.name.val : "call")
     for a in x.args
-        show(io, a, indent+1, cnt)
-        cnt+=a.span
+        show(io, a, indent+1)
     end 
 end
 
-function Base.show(io::IO, x::KEYWORD_BLOCK{0},indent=0, pos=0)
+function Base.show(io::IO, x::KEYWORD_BLOCK{0},indent=0)
     println(io, "⊢","-"^indent, x.opener.val)
 end
 
-function Base.show(io::IO, x::KEYWORD_BLOCK,indent=0, pos=0)
+function Base.show(io::IO, x::KEYWORD_BLOCK,indent=0)
     println(io, "⊢","-"^indent, x.opener.val)
 end
+function Base.show(io::IO, x::CURLY,indent=0)
+    println(io, "⊢","-"^indent, "{}")
+    for a in x.args
+        show(io, a, indent+1)
+    end 
+end
 
-function Base.show(io::IO, x::BLOCK,indent=0, pos=0)
-    cnt = 0
+function Base.show(io::IO, x::BLOCK,indent=0)
     for a in x.args[1:end]
-        show(io, a, indent, pos)
-        cnt+=a.span
+        show(io, a, indent)
     end   
 end
 
-function Base.show(io::IO, x::KEYWORD_BLOCK{3},indent=0, pos=0)
+function Base.show(io::IO, x::KEYWORD_BLOCK{3},indent=0)
     println(io, "⊢","-"^indent, x.opener.val)
-    cnt = 0
     for a in x.args[2:end]
-        show(io, a, indent+1, cnt)
-        cnt+=a.span
+        show(io, a, indent+1)
     end 
 end
 
