fe78157a0f9ca8b890c504f8d45b517fa46b1102
Zac Nugent <zac.nugent@gmail.com>
2017-01-31 16:21:21 +0000

    move ws/comment concat from Tokenize to Parser

--- src/parsestate.jl
+++ src/parsestate.jl
@@ -1,3 +1,5 @@
+import Tokenize.Lexers: peekchar, prevchar, readchar, iswhitespace, emit, emit_error, backup!, accept_batch
+
 const empty_whitespace = Token()
 
 type ParseState
@@ -42,10 +44,59 @@ function next(ps::ParseState)
     ps.lws = ps.ws
     ps.ws = ps.nws
     ps.nt, ps.done  = next(ps.l, ps.done)
-    if iswhitespace(peekchar(ps.l))
-        ps.nws, ps.done = next(ps.l, ps.done)
+    if iswhitespace(peekchar(ps.l)) || peekchar(ps.l)=='#'
+        readchar(ps.l)
+        ps.nws = lex_ws_comment(ps.l)
     else
-        ps.nws = empty_whitespace
+        ps.nws = Parser.empty_whitespace
     end
     return ps
+end
+
+function lex_ws_comment(l::Lexer)
+    if prevchar(l)=='#'
+        read_comment(l)
+    else
+        accept_batch(l, iswhitespace)
+    end
+    while iswhitespace(peekchar(l)) || peekchar(l)=='#'
+        readchar(l)
+        if prevchar(l)=='#'
+            read_comment(l)
+        else
+            accept_batch(l, iswhitespace)
+        end
+    end
+
+    return emit(l, Tokens.WHITESPACE)
+end
+
+function read_comment(l::Lexer)
+    if readchar(l) != '='
+        while true
+            c = readchar(l)
+            if c == '\n' || eof(c)
+                backup!(l)
+                break
+            end
+        end
+    else
+        c = readchar(l) # consume the '='
+        n_start, n_end = 1, 0
+        while true
+            if eof(c)
+                return emit_error(l, Tokens.EOF_MULTICOMMENT)
+            end
+            nc = readchar(l)
+            if c == '#' && nc == '='
+                n_start += 1
+            elseif c == '=' && nc == '#'
+                n_end += 1
+            end
+            if n_start == n_end
+                break
+            end
+            c = nc
+        end
+    end
 end
