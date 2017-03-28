7693e9c5a8f8fbffad49776be572a53f3c801367
Zac Nugent <zac.nugent@gmail.com>
2017-03-28 17:22:10 +0000

    deprecations

---
+++ src/deprecations.jl
@@ -0,0 +1,24 @@
+dep = parse(readstring(Base.functionloc(Base.depwarn)[1]),true)
+
+deprecated_symbols = []
+
+for x in dep
+    if x isa EXPR && x.head == MACROCALL && x.args[1] isa IDENTIFIER && (x.args[1].val==Symbol("@deprecate") || x.args[1].val==Symbol("@deprecate_binding") )
+        old = x.args[2]
+        if old isa IDENTIFIER
+            push!(deprecated_symbols, old.val)
+        elseif x isa EXPR && x.head == CALL
+            push!(deprecated_symbols, _get_fname(old).val)
+        end
+    # elseif x isa EXPR && x.head isa KEYWORD{Tokens.FOR}
+    #     v = x[2][1].val
+    #     list = Expr.(x[2][3])
+    end
+end
+
+function is_codegen_loop(x)
+    if x isa EXPR && x.head isa KEYWORD{Tokens.FOR} && ((x[2].head == CALL && x[2].args[1] isa OPERATOR{6, Tokens.IN}) || (x[2].head isa OPERATOR{1,Tokens.EQ}) && )
+
+    end
+    false
+end
