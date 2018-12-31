2e230897d351b1e5e5b5d2ef1cbf02955feac80c
ZacNugent <zac.nugent@gmail.com>
2018-12-31 20:25:34 +0000

    julia 1.1 tuple fix

--- src/components/lists.jl
+++ src/components/lists.jl
@@ -6,9 +6,10 @@ tuple.
 """
 function parse_tuple(ps::ParseState, @nospecialize(ret))
     op = PUNCTUATION(next(ps))
-
-    if closer(ps) || (isassignment(ps.nt) && ps.nt.kind != Tokens.APPROX)
+    if (isassignment(ps.nt) && ps.nt.kind != Tokens.APPROX)
         ret = EXPR{TupleH}(Any[ret, op])
+    elseif closer(ps)
+        ret = ErrorToken(EXPR{TupleH}(Any[ret, op]))
     else
         nextarg = @closer ps tuple parse_expression(ps)
         ret = EXPR{TupleH}(Any[ret, op, nextarg])
