26745ae2ef7246a3be65a51cff765e86a8f4dd94
Sebastian Pfitzner <pfitzseb@physik.hu-berlin.de>
2019-08-13 19:06:57 +0200

    consistently print to `io`

--- src/display.jl
+++ src/display.jl
@@ -3,9 +3,9 @@ function Base.show(io::IO, x::EXPR, d = 0, er = false)
     c =  T === ErrorToken || er ? :red : :normal
     if isidentifier(x)
         printstyled(io, " "^d, valof(x), "  ", x.fullspan, "(", x.span, ")", color = :yellow)
-        bindingof(x) !== nothing && printstyled(" $(bindingof(x).name)", bindingof(x).t === nothing ? "" : "::", color = :blue)
-        refof(x) !== nothing && printstyled(" * ", color = :red)
-        println()
+        bindingof(x) !== nothing && printstyled(io, " $(bindingof(x).name)", bindingof(x).t === nothing ? "" : "::", color = :blue)
+        refof(x) !== nothing && printstyled(io, " * ", color = :red)
+        println(io)
     elseif isoperator(x)
         printstyled(io, " "^d, "OP: ", kindof(x), "  ", x.fullspan, "(", x.span, ")\n", color = c)
     elseif iskw(x)
@@ -30,8 +30,8 @@ function Base.show(io::IO, x::EXPR, d = 0, er = false)
         printstyled(io, " "^d, T, "  ", x.fullspan, "(", x.span, ")", color = c)
         scopeof(x) !== nothing && printstyled(io, " new scope", color = :green)
         bindingof(x) !== nothing && printstyled(io, " $(bindingof(x).name)", bindingof(x).t === nothing ? "" : "::", color = :blue)
-        refof(x) !== nothing && printstyled(" * ", color = :red)
-        println()
+        refof(x) !== nothing && printstyled(io, " * ", color = :red)
+        println(io)
         x.args === nothing && return
         for a in x.args
             show(io, a, d + 1, er)
@@ -44,5 +44,3 @@ function Base.show(io::IO, scope::Scope)
     println(io, scope.names isa Dict ? string("[", join(collect(keys(scope.names)), ","), "]") : "[]")
     println(io, scope.modules isa Dict ? string("[", join(collect(keys(scope.modules)), ","), "]") : "[]")
 end
-
-
