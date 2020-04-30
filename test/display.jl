3b13bfbbb6c1e8929292592546c1a970191728c7
Chris Foster <chris42f@gmail.com>
2020-04-30 15:59:03 +1000

    show(::EXPR) refinement: print range in source
    Print EXPR (full)spans as 1-based range of the source string. This
    presentation is simple to understand and use when considering EXPR spans
    as an overlay of the input string.

--- test/display.jl
+++ test/display.jl
@@ -1,18 +1,19 @@
 @testset "show" begin
     x = CSTParser.parse("a + (b*c) - d")
-    @test sprint(show, x) === """
-    0   13  13 | BinaryOpCall
-    0   10  9  |  BinaryOpCall
-    0   2   1  |   a
-    2   2   1  |   OP: PLUS
-    4   6   5  |   InvisBrackets
-    4   1   1  |    (
-    5   3   3  |    BinaryOpCall
-    5   1   1  |     b
-    6   1   1  |     OP: STAR
-    7   1   1  |     c
-    8   2   1  |    )
-    10  2   1  |  OP: MINUS
-    12  1   1  |  d
+    @test sprint(show, x) ===
+    """
+      1:13  BinaryOpCall
+      1:10   BinaryOpCall
+      1:2     a
+      3:4     OP: PLUS
+      5:10    InvisBrackets
+      5:5      (
+      6:8      BinaryOpCall
+      6:6       b
+      7:7       OP: STAR
+      8:8       c
+      9:10     )
+     11:12   OP: MINUS
+     13:13   d
     """
 end
