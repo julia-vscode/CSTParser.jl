7af14556274b437cbe9f1eeadc149fb70a4d6c4c
Zac Nugent <zac.nugent@gmail.com>
2021-01-14 20:03:02 +0000

    make sure everything handles TRIPLESTRINGs

--- src/components/strings.jl
+++ src/components/strings.jl
@@ -142,7 +142,7 @@ function parse_string_or_cmd(ps::ParseState, prefixed=false)
                     rparen = EXPR(:RPAREN, 1, 1)
 
                     ps1 = ParseState(input)
-                    ps1.lastbyte = ps.t.endbyte
+                    ps1.lastbyte = ps.t.endbyte - (istrip ? 3 : 1)
                     # We're reusing a portion of the string from `ps` so we need to make sure `ps1` knows where the end of the string is.
 
                     if kindof(ps1.nt) === Tokens.RPAREN
